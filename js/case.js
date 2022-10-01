21a33c1001c9bad85403eb1c699130fe8852561e
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-10-01 23:44:37 +0600

    catching minus button and number field ctached by js

--- js/case.js
+++ js/case.js
@@ -1,8 +1,36 @@
-document.getElementById('btn-case-plus').addEventListener('click', function(){
+/*
+1 add event lostener to the case plus button
+2 get the value inside the case number field (input field)
+3 convert the number to an interger
+4 new case number
+5 save the value to the case number field
+*/
+function updateCaseNumber(isIncrease){
     const caseNumberField = document.getElementById('case-number-field');
-    const caseNumberString = caseNumberField.ariaValueMax;
+    const caseNumberString = caseNumberField.value;
     const previousCaseNumber = parseFloat(caseNumberString);
 
-    const newCaseNumber = previousCaseNumber + 1;
+    let newCaseNumber;
+
+    if(isIncrease == true){
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else{
+        newCaseNumber = previousCaseNumber - 1;
+    }
     caseNumberField.value = newCaseNumber;
+}
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    updateCaseNumber(true);
+})
+
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    updateCaseNumber(false);
+    // const caseNumberField = document.getElementById('case-number-field');
+    // const caseNumberString = caseNumberField.value;
+    // const previousCaseNumber = parseFloat(caseNumberString);
+
+    // const newCaseNumber = previousCaseNumber - 1;
+    // caseNumberField.value = newCaseNumber;
 })
